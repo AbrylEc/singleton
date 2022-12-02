2134e77ac0bd4005fe1c61a50728700efb4cb7ad
AbrylEc <abrylmusic@hotmail.com>
2022-12-02 07:48:27 -0500

    init commit

---
+++ src/main/java/ec/edu/itsqmet/factory/Car.java
@@ -0,0 +1,18 @@
+package ec.edu.itsqmet.factory;
+
+public abstract class Car {
+	private int year;
+	
+	public int getYear() { //retorna un valor 
+		return year;
+	}
+	
+	public void setYear(int year) { //set-modificar la informacion 
+		this.year=year;
+	}
+	
+	public String maxSpeed() {
+		return "0 km/h";		
+	}
+
+}
